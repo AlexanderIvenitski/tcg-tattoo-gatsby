0b83fe61d2fb94d5d547c972e0e3235c079488f4
AlexanderIvenitski <alexanderivenitski@gmail.com>
2023-07-08 10:25:10 +0200

    add query

--- src/components/faq.js
+++ src/components/faq.js
@@ -1,18 +1,42 @@
 import React from 'react';
 import { Row, Accordion} from 'react-bootstrap';
+import { graphql } from "gatsby"
+
+
 
 
 const Faq = ({data}) => (
     <Row>
         <Accordion>
+        {data.faq.edges.map(({node}) => (
           <Accordion.Item>
-            <Accordion.Header>test</Accordion.Header>
+            <Accordion.Header>
+            {node.frontmatter.title}
+            </Accordion.Header>
               <Accordion.Body>
                 test
               </Accordion.Body>
             </Accordion.Item> 
+        ))}
         </Accordion>
     </Row>
 )
 
 export default Faq;
+
+export const query = graphql`
+  query {
+
+    faq: allMarkdownRemark(filter: {html: {ne: ""}}) {
+      edges {
+        node {
+          html
+          id
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
+  }
+`
